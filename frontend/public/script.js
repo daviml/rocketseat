209798ee8eae8554540efbc15c25fbc7ac0e2f3f
Davi Moraes Ladeira <davimoraesladeira@hotmail.com>
2020-11-13 21:19:11 -0300

    Up

---
+++ frontend/public/script.js
@@ -0,0 +1,5 @@
+Babel: converte codiogo do react para o codigo entender
+
+webpack: para cada tipo de carquivo eu vou converter o codigo de maneira diferente
+
+loaders: babel-loader, css-loader, image-loader
