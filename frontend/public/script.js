9241c14df8a5ec00872f4d95a4bdabb7f99e393e
Davi Moraes Ladeira <davimoraesladeira@hotmail.com>
2022-03-03 18:33:53 -0300

    delete trash

--- frontend/public/script.js
+++
@@ -1,5 +0,0 @@
-Babel: converte codiogo do react para o codigo entender
-
-webpack: para cada tipo de carquivo eu vou converter o codigo de maneira diferente
-
-loaders: babel-loader, css-loader, image-loader
